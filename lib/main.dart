5e02f0689765decf78c0f8820d2b7ecaa709d592
hmody-web <hmode.qq@gmail.com>
2026-05-02 00:17:45 +0300

    add icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
   runApp(const MyApp());
 }
 
@@ -26,19 +31,46 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
   late final WebViewController controller;
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
+
     controller = WebViewController()
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (url) {
+            setState(() => isLoading = true);
+          },
+          onPageFinished: (url) {
+            setState(() => isLoading = false);
+          },
+        ),
+      )
       ..loadRequest(Uri.parse("https://majidalbana.com")); 
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: WebViewWidget(controller: controller),
+      body: Stack(
+        children: [
+          SizedBox.expand(
+            child: WebViewWidget(controller: controller),
+          ),
+
+          // 🔥 Loading احترافي
+          if (isLoading)
+            Container(
+              color: const Color.fromARGB(255, 0, 2, 10),
+              child: const Center(
+                child: CircularProgressIndicator(
+                  color: Color.fromARGB(255, 204, 134, 30),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
